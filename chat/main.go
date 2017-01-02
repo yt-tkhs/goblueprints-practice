2247a7ac6d6a30f1c64d99a30cd18062fc2d74f0
Yuta Takahashi <yt.fp.0130@gmail.com>
2017-01-02 14:42:51 +0900

    Use template

--- chat/main.go
+++ chat/main.go
@@ -3,22 +3,29 @@ package main
 import (
 	"net/http"
 	"github.com/labstack/gommon/log"
+	"sync"
+	"text/template"
+	"path/filepath"
 )
+
+type templateHandler struct {
+	once        sync.Once
+	fileName    string
+	templ       *template.Template
+}
+
+// Process HTTP Request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)  {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.fileName)))
+	})
+	t.templ.Execute(w, nil)
+}
 
 func main() {
 
 	// When you access http://localhost:8080/, following function is executed.
-	http.HandleFunc("/", func(w http.ResponseWriter, r*http.Request) {
-		w.Write([]byte(`
-			<html>
-			  <head>
-			    <title>Chat</title>
-			  </head>
-			  <body>
-			    Let's chat!
-			  </body>
-		`))
-	})
+	http.Handle("/", &templateHandler{fileName: "index.html"})
 
 	// Start Web Server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
