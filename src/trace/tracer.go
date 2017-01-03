bbb01170cb4878f17c62e15ed8bf49b8ab78dcbe
Yuta Takahashi <yt.fp.0130@gmail.com>
2017-01-03 19:49:40 +0900

    Impl nilTracer

--- src/trace/tracer.go
+++ src/trace/tracer.go
@@ -13,11 +13,21 @@ type tracer struct {
 	out io.Writer
 }
 
+type nilTracer struct {}
+
 func (t *tracer) Trace(a ...interface{}) {
 	t.out.Write([]byte(fmt.Sprint(a...)))
 	t.out.Write([]byte("\n"))
 }
 
+func (t *nilTracer) Trace(a ...interface{}) {
+	// do nothing
+}
+
 func New(w io.Writer) Tracer {
 	return &tracer{out: w}
+}
+
+func Off() Tracer {
+	return &nilTracer{}
 }
