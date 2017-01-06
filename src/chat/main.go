a36b8916198ea786cc537ef48fadc02df5491998
Yuta Takahashi <yt.fp.0130@gmail.com>
2017-01-06 14:54:36 +0900

    Impl Log-out

--- src/chat/main.go
+++ src/chat/main.go
@@ -56,6 +56,17 @@ func main() {
 	http.Handle("/chat", MustAuth(&templateHandler{fileName: "index.html"}))
 	http.Handle("/login", &templateHandler{fileName: "login.html"})
 	http.HandleFunc("/auth/", loginHandler)
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name: "auth",
+			Value: "",
+			Path: "/",
+			MaxAge: -1,
+		})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
+
 	http.Handle("/room", r)
 
 	go r.run()
